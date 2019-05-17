fac22155ebcca170077163f13c4c958e928c7ba4
Milad Noroozi <miladn@cafebazaar.ir>
2019-05-17 12:26:03 +0430

    [TASK] Implementation of GRPC Server

--- internal/server/grpc.go
+++ internal/server/grpc.go
@@ -2,8 +2,10 @@ package server
 
 import (
 	"context"
+	"io"
 
 	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/pkg/errors"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
@@ -11,7 +13,8 @@ import (
 )
 
 type grpcServicer struct {
-	broker ubroker.Broker
+	broker   ubroker.Broker
+	delivery <-chan *ubroker.Delivery
 }
 
 func NewGRPC(broker ubroker.Broker) ubroker.BrokerServer {
@@ -21,17 +24,133 @@ func NewGRPC(broker ubroker.Broker) ubroker.BrokerServer {
 }
 
 func (s *grpcServicer) Fetch(stream ubroker.Broker_FetchServer) error {
-	return status.Error(codes.Unimplemented, "not implemented")
+	var err error
+	ctx := stream.Context()
+
+	select {
+	case <-ctx.Done():
+		return s.handleError(ctx.Err())
+	default:
+		s.delivery, err = s.broker.Delivery(ctx)
+		if err != nil {
+			return s.handleError(err)
+		}
+	}
+
+	for {
+		_, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			continue
+		}
+
+		select {
+		case <-ctx.Done():
+			return s.handleError(err)
+		case msg := <- s.delivery:
+			if msg == nil {
+				return status.Error(codes.Unavailable, "Empty Queue")
+			}
+			if err := stream.Send(msg); err != nil {
+				return s.handleError(err)
+			}
+		}
+	}
 }
 
 func (s *grpcServicer) Acknowledge(ctx context.Context, request *ubroker.AcknowledgeRequest) (*empty.Empty, error) {
-	return &empty.Empty{}, status.Error(codes.Unimplemented, "not implemented")
+	err := s.broker.Acknowledge(ctx, request.Id)
+	return &empty.Empty{}, s.handleError(err)
 }
 
 func (s *grpcServicer) ReQueue(ctx context.Context, request *ubroker.ReQueueRequest) (*empty.Empty, error) {
-	return &empty.Empty{}, status.Error(codes.Unimplemented, "not implemented")
+	err := s.broker.ReQueue(ctx, request.Id)
+	return &empty.Empty{}, s.handleError(err)
 }
 
 func (s *grpcServicer) Publish(ctx context.Context, request *ubroker.Message) (*empty.Empty, error) {
-	return &empty.Empty{}, status.Error(codes.Unimplemented, "not implemented")
+	err := s.broker.Publish(ctx, request)
+	return &empty.Empty{}, s.handleError(err)
+}
+
+func (s *grpcServicer) handleError(err error) error {
+	return status.Error(s.errorToStatusCode(err), s.errorToMessage(err))
+}
+
+func (s *grpcServicer) errorToStatusCode(err error) codes.Code {
+	if err == nil {
+		return codes.OK
+	}
+	for {
+		result, ok := s.tryErrorToStatusCode(err)
+		if ok {
+			return result
+		}
+
+		cause := errors.Cause(err)
+		if cause == err {
+			return result
+		}
+
+		err = cause
+	}
+}
+
+func (s *grpcServicer) tryErrorToStatusCode(err error) (codes.Code, bool) {
+	switch err {
+	case ubroker.ErrClosed:
+		return codes.Unavailable, true
+
+	case ubroker.ErrUnimplemented:
+		return codes.Unimplemented, true
+
+	case ubroker.ErrInvalidID, errInvalidArgument:
+		return codes.InvalidArgument, true
+
+	case context.Canceled, context.DeadlineExceeded:
+		return codes.DeadlineExceeded, true
+
+	default:
+		return codes.Internal, false
+	}
+}
+
+func (s *grpcServicer) errorToMessage(err error) string {
+	if err == nil {
+		return "OK"
+	}
+	for {
+		result, ok := s.tryErrorToMessage(err)
+		if ok {
+			return result
+		}
+
+		cause := errors.Cause(err)
+		if cause == err {
+			return result
+		}
+
+		err = cause
+	}
+}
+
+func (s *grpcServicer) tryErrorToMessage(err error) (string, bool) {
+	switch err {
+	case ubroker.ErrClosed:
+		return "Closed", true
+
+	case ubroker.ErrUnimplemented:
+		return "Unimplemented", true
+
+	case ubroker.ErrInvalidID:
+		return "InvalidID", true
+
+	case context.Canceled, context.DeadlineExceeded:
+		return "DeadlineExceeded", true
+
+	default:
+		return "InternalError", false
+	}
 }
